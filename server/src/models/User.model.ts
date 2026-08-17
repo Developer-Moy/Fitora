865d343c33da110fec56d0b201c7c6b47ef23c75
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-08-17 12:10:41 +0600

    created user mongoose schema

--- server/src/models/User.model.ts
+++ server/src/models/User.model.ts
@@ -1,1 +1,39 @@
-export {};
+import mongoose, { Document, Schema } from "mongoose"
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  passwordHash: string;
+  role: "user" | "admin";
+}
+
+const userSchema = newSchema<IUser>(
+  {
+    name: {
+      type: string,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: string,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: string,
+      required: true,
+    },
+    role: {
+      type: string,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model<IUser>("user", userSchema);
