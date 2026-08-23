9e44e4c5486640f0127d32b9bf836d88eadfaa26
Gazi Md Salauddin <mdsalauddin329132@gmail.com>
2026-08-23 18:39:50 +0600

    [Backend] Dynamic Goals CRUD API & Workout Streak Engine

--- server/src/routes/goal.routes.ts
+++ server/src/routes/goal.routes.ts
@@ -1,1 +1,20 @@
-export {};
+import { Router } from "express";
+
+import {
+  createOrUpdateGoal,
+  getGoal,
+  updateGoal,
+  deleteGoal,
+} from "../controllers/goal.controller";
+
+const router = Router();
+
+router.post("/", createOrUpdateGoal);
+
+router.get("/:userId", getGoal);
+
+router.patch("/:id", updateGoal);
+
+router.delete("/:id", deleteGoal);
+
+export default router;
