b2be1e27ca862dea3ab9ac5b5508376beb6af5f5
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-08-24 16:47:17 +0600

    [ADDED] Coaches Section

---
+++ client/src/components/home/TrainersSection.tsx
@@ -0,0 +1,11 @@
+import CoachesSection from '@/sections/Coaches&Trainers/Coaches';
+
+const TrainersSection = () => {
+    return (
+        <div>
+            <CoachesSection />
+        </div>
+    );
+};
+
+export default TrainersSection;
